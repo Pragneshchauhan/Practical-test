2887e85360ed725baaf2bbd5861be3a4ac52f6e8
Pragneshchauhan <71453132+Pragneshchauhan@users.noreply.github.com>
2021-06-01 15:02:16 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -7,22 +7,33 @@ from django.views.decorators.csrf import csrf_exempt
 def index(reuest):
     customers = Customer.objects.all()
     products = Product.objects.all()
+    
     return render(reuest,'myapp/index.html',{'all_customers':customers,'all_products':products})
 
 @csrf_exempt
 def save_data(request):
+    
     if request.method == 'POST':
+        
         customer_id = request.POST['customer_id']
+        
         product_id = request.POST['product_id']
         print("==================",product_id)
+        
         myshowPrice = request.POST['myshowPrice']
+        
         qty = request.POST['qty']
+        
         total = request.POST['total']
+        
         print(customer_id)
+        
         customer_id = Customer.objects.get(id=int(customer_id))
+        
         product_id = Product.objects.get(id=int(product_id))
         print("==================",customer_id)
         print("==================",product_id)
+        
         Order.objects.create(
             customer_Id=customer_id,product_Id=product_id,unit_Price=myshowPrice,Quntity=qty,total_Price=total)
         return JsonResponse({'status':'Success'})
